54abbb774b53910224a761b848fa3b54531422aa
vatsankit123 <vatsaankit7281@gmail.com>
2025-03-11 17:10:09 +0000

    push

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144555
+let accountEmail="ankit@gmail.com"
+var accountpass="1234"
+accountcity="bengaluru"
+
+//accountId=2; // not allowed
+
+accountEmail="abdul@gmail.com"     // this is allowed
+
+accountpass="21212"            // this is also allowed 
+
+accountcity="patna"
+
+// console.log(accountcity);
+
+// bar bar console.log likhne se behtar hai ham console.table ka use karein
+
+console.table([accountEmail,accountId,accountpass,accountcity])  // sara data tabular form me dikhega
