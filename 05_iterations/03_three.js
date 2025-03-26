b242f4ddad86f55c1405522ff915d0d29f9dd4fe
vatsankit123 <vatsaankit7281@gmail.com>
2025-03-26 13:50:33 +0000

    iterations

--- 05_iterations/03_three.js
+++ 05_iterations/03_three.js
@@ -21,9 +21,26 @@ console.log(num);
 
  // Maps in javascript
 
- const map = mew Map()
+ const map = new Map()
  map.set('IN' , 'India')
  map.set('USA', ' united states of america')
  map.set('FR','France')
 
- console.log(map);
+ // console.log(map);
+
+ // iterating in map
+
+ for(const [key,value] of map){
+   console.log(key, ':-',value);
+ }
+
+ // as we know map and object store in key value pair but still in object iteration is diffrent from map 
+
+ const myobject = {
+   'game1' : 'GTA',
+   'game2' : 'BGMI'
+ }
+
+ for(const [key , value] of myobject){     // this will through error that myobject is not iterable
+   console.log(key , ':-',value);
+ }
